7af33b54b9ccdafab8fba0545d4a6173a064f33d
GuilhermeFS120 <guilhermefs120@gmail.com>
2023-09-05 17:44:05 -0300

    update v1.22

---
+++ js/dist/script.dev.js
@@ -0,0 +1,184 @@
+"use strict";
+
+var Modal = {
+  toggle: function toggle(modal) {
+    document.querySelector(modal).classList.toggle('active');
+  }
+};
+var Storage = {
+  get: function get() {
+    return JSON.parse(localStorage.getItem('dev.finances:transactions')) || [];
+  },
+  set: function set(transactions) {
+    localStorage.setItem('dev.finances:transactions', JSON.stringify(transactions));
+  }
+};
+var Transaction = {
+  all: Storage.get(),
+  add: function add(transaction) {
+    Transaction.all.push(transaction);
+    App.reload();
+  },
+  remove: function remove(index) {
+    Transaction.all.splice(index, 1);
+    App.reload();
+  },
+  incomes: function incomes() {
+    var income = 0;
+    Transaction.all.forEach(function (transaction) {
+      if (transaction.amount > 0) {
+        income += transaction.amount;
+      }
+    });
+    return income;
+  },
+  expenses: function expenses() {
+    var expense = 0;
+    Transaction.all.forEach(function (transaction) {
+      if (transaction.amount < 0) {
+        expense += transaction.amount;
+      }
+    });
+    return expense;
+  },
+  total: function total() {
+    var income = Transaction.incomes();
+    var expense = Transaction.expenses();
+    return income + expense;
+  }
+};
+var DOM = {
+  transactionsContainer: document.querySelector('#data-table tbody'),
+  addTransaction: function addTransaction(transaction, index) {
+    var tr = document.createElement('tr');
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
+    DOM.transactionsContainer.appendChild(tr);
+  },
+  innerHTMLTransaction: function innerHTMLTransaction(transaction, index) {
+    var CSSclass = transaction.amount > 0 ? 'income' : 'expense';
+    var amount = Utils.formatCurrency(transaction.amount);
+    var html = "\n            <td class=\"description\">".concat(transaction.description, "</td>\n            <td class=\"").concat(CSSclass, "\">").concat(amount, "</td>\n            <td class=\"date\">").concat(transaction.date, "</td>\n            <td class=\"remove\">\n                <img class=\"remove button\" onclick=\"Transaction.remove(").concat(index, ")\" src=\"./assets/minus.svg\" alt=\"Remover Transa\xE7\xE3o\">\n            </td>\n        ");
+    return html;
+  },
+  updateBalance: function updateBalance() {
+    var income = Transaction.incomes();
+    var expense = Transaction.expenses();
+    var total = Transaction.total();
+    document.getElementById('incomeDisplay').innerHTML = Utils.formatCurrency(income);
+    document.getElementById('expenseDisplay').innerHTML = Utils.formatCurrency(expense);
+    document.getElementById('totalDisplay').innerHTML = Utils.formatCurrency(total);
+  },
+  clearTransactions: function clearTransactions() {
+    DOM.transactionsContainer.innerHTML = '';
+  }
+};
+var Utils = {
+  formatAmount: function formatAmount(value) {
+    value = Number(value) * 100;
+    return value;
+  },
+  formatDate: function formatDate(date) {
+    var splittedDate = date.split('-');
+    return "".concat(splittedDate[2], "/").concat(splittedDate[1], "/").concat(splittedDate[0]);
+  },
+  formatCurrency: function formatCurrency(value) {
+    var signal = Number(value) < 0 ? '-' : '';
+    value = String(value).replace(/\D/g, '');
+    value = Number(value) / 100;
+    value = value.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL'
+    });
+    return signal + value;
+  }
+};
+var Form = {
+  description: document.querySelector('input#description'),
+  amount: document.querySelector('input#amount'),
+  date: document.querySelector('input#date'),
+  getValues: function getValues() {
+    return {
+      description: Form.description.value,
+      amount: Form.amount.value,
+      date: Form.date.value
+    };
+  },
+  validateField: function validateField() {
+    var _Form$getValues = Form.getValues(),
+        description = _Form$getValues.description,
+        amount = _Form$getValues.amount,
+        date = _Form$getValues.date;
+
+    if (description.trim() === '' || amount.trim() === '' || date.trim() === '') {
+      throw new Error('Por favor, preencha todos os campos.');
+    }
+  },
+  formatValues: function formatValues() {
+    var _Form$getValues2 = Form.getValues(),
+        description = _Form$getValues2.description,
+        amount = _Form$getValues2.amount,
+        date = _Form$getValues2.date;
+
+    amount = Utils.formatAmount(amount);
+    date = Utils.formatDate(date);
+    return {
+      description: description,
+      amount: amount,
+      date: date
+    };
+  },
+  clearFields: function clearFields() {
+    Form.description.value = '';
+    Form.amount.value = '';
+    Form.date.value = '';
+  },
+  submit: function submit(event) {
+    event.preventDefault();
+
+    try {
+      Form.validateField();
+      var transaction = Form.formatValues();
+      Transaction.add(transaction);
+      Form.clearFields();
+      Modal.toggle('.modal-overlay');
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+};
+var App = {
+  init: function init() {
+    Transaction.all.forEach(DOM.addTransaction);
+    DOM.updateBalance();
+    Storage.set(Transaction.all);
+    var theme = localStorage.getItem('dev.finances:theme') || 'green',
+        DM = localStorage.getItem('dev.finances:dark-mode') || 'off',
+        dmInput = document.getElementById('darkMode');
+    dmInput.checked = DM == 'on' ? true : false;
+    document.documentElement.setAttribute('dark-mode', DM);
+    document.querySelector('#DM label').innerHTML = DM == 'on' ? 'Ligado' : 'Desligado';
+    dmInput.addEventListener('click', function (e) {
+      document.documentElement.setAttribute('dark-mode', e.target.checked ? 'on' : 'off');
+      document.querySelector('#DM label').innerHTML = e.target.checked ? 'Ligado' : 'Desligado';
+      localStorage.setItem('dev.finances:dark-mode', e.target.checked ? 'on' : 'off');
+    });
+    document.documentElement.setAttribute('data-theme', theme);
+  },
+  reload: function reload() {
+    DOM.clearTransactions();
+    this.init();
+  }
+};
+var Custom = {
+  setTheme: function setTheme(theme) {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('dev.finances:theme', theme);
+  },
+  toggleDM: function toggleDM(darkmode) {
+    document.documentElement.setAttribute('dark-mode', darkmode ? 'on' : 'off');
+    document.querySelector('#DM label').innerHTML = darkmode ? 'Ligado' : 'Desligado';
+    localStorage.setItem('dev.finances:dark-mode', darkmode);
+  }
+};
+App.init();
--- js/script.js
+++ js/script.js
@@ -1,71 +1,73 @@
 const Modal = {
-  toggle(modal) {
-    document.querySelector(modal).classList.toggle('active')
-  }
+    toggle(modal) {
+        document.querySelector(modal).classList.toggle('active')
+    }
 }
 
 const Storage = {
-  get() {
-    return JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
-  },
-  set(transactions) {
-    localStorage.setItem(
-      'dev.finances:transactions',
-      JSON.stringify(transactions)
-    )
-  }
+    get() {
+        return (
+            JSON.parse(localStorage.getItem('dev.finances:transactions')) || []
+        )
+    },
+    set(transactions) {
+        localStorage.setItem(
+            'dev.finances:transactions',
+            JSON.stringify(transactions)
+        )
+    }
 }
 
 const Transaction = {
-  all: Storage.get(),
+    all: Storage.get(),
 
-  add(transaction) {
-    Transaction.all.push(transaction)
-    App.reload()
-  },
-  remove(index) {
-    Transaction.all.splice(index, 1)
-    App.reload()
-  },
-  incomes() {
-    let income = 0
-    Transaction.all.forEach(transaction => {
-      if (transaction.amount > 0) {
-        income += transaction.amount
-      }
-    })
-    return income
-  },
-  expenses() {
-    let expense = 0
-    Transaction.all.forEach(transaction => {
-      if (transaction.amount < 0) {
-        expense += transaction.amount
-      }
-    })
-    return expense
-  },
-  total() {
-    let income = Transaction.incomes()
-    let expense = Transaction.expenses()
+    add(transaction) {
+        Transaction.all.push(transaction)
+        App.reload()
+    },
+    remove(index) {
+        Transaction.all.splice(index, 1)
+        App.reload()
+    },
+    incomes() {
+        let income = 0
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount
+            }
+        })
+        return income
+    },
+    expenses() {
+        let expense = 0
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount
+            }
+        })
+        return expense
+    },
+    total() {
+        let income = Transaction.incomes()
+        let expense = Transaction.expenses()
 
-    return income + expense
-  }
+        return income + expense
+    }
 }
 
 const DOM = {
-  transactionsContainer: document.querySelector('#data-table tbody'),
+    transactionsContainer: document.querySelector('#data-table tbody'),
 
-  addTransaction(transaction, index) {
-    const tr = document.createElement('tr')
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
-    tr.dataset.index = index
-    DOM.transactionsContainer.appendChild(tr)
-  },
-  innerHTMLTransaction(transaction, index) {
-    const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
-    const amount = Utils.formatCurrency(transaction.amount)
-    const html = `
+    addTransaction(transaction, index) {
+        const tr = document.createElement('tr')
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction, index)
+        tr.dataset.index = index
+        DOM.transactionsContainer.appendChild(tr)
+    },
+    innerHTMLTransaction(transaction, index) {
+        const CSSclass = transaction.amount > 0 ? 'income' : 'expense'
+        const amount = Utils.formatCurrency(transaction.amount)
+        const html = `
             <td class="description">${transaction.description}</td>
             <td class="${CSSclass}">${amount}</td>
             <td class="date">${transaction.date}</td>
@@ -74,122 +76,150 @@ const DOM = {
             </td>
         `
 
-    return html
-  },
-  updateBalance() {
-    let income = Transaction.incomes()
-    let expense = Transaction.expenses()
-    let total = Transaction.total()
+        return html
+    },
+    updateBalance() {
+        let income = Transaction.incomes()
+        let expense = Transaction.expenses()
+        let total = Transaction.total()
 
-    document.getElementById('incomeDisplay').innerHTML =
-      Utils.formatCurrency(income)
-    document.getElementById('expenseDisplay').innerHTML =
-      Utils.formatCurrency(expense)
-    document.getElementById('totalDisplay').innerHTML =
-      Utils.formatCurrency(total)
-  },
-  clearTransactions() {
-    DOM.transactionsContainer.innerHTML = ''
-  }
+        document.getElementById('incomeDisplay').innerHTML =
+            Utils.formatCurrency(income)
+        document.getElementById('expenseDisplay').innerHTML =
+            Utils.formatCurrency(expense)
+        document.getElementById('totalDisplay').innerHTML =
+            Utils.formatCurrency(total)
+    },
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ''
+    }
 }
 
 const Utils = {
-  formatAmount(value) {
-    value = Number(value) * 100
-    return value
-  },
-  formatDate(date) {
-    const splittedDate = date.split('-')
-    return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
-  },
-  formatCurrency(value) {
-    const signal = Number(value) < 0 ? '-' : ''
-    value = String(value).replace(/\D/g, '')
-    value = Number(value) / 100
-    value = value.toLocaleString('pt-BR', {
-      style: 'currency',
-      currency: 'BRL'
-    })
+    formatAmount(value) {
+        value = Number(value) * 100
+        return value
+    },
+    formatDate(date) {
+        const splittedDate = date.split('-')
+        return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+    },
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? '-' : ''
+        value = String(value).replace(/\D/g, '')
+        value = Number(value) / 100
+        value = value.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
 
-    return signal + value
-  }
+        return signal + value
+    }
 }
 
 const Form = {
-  description: document.querySelector('input#description'),
-  amount: document.querySelector('input#amount'),
-  date: document.querySelector('input#date'),
+    description: document.querySelector('input#description'),
+    amount: document.querySelector('input#amount'),
+    date: document.querySelector('input#date'),
 
-  getValues() {
-    return {
-      description: Form.description.value,
-      amount: Form.amount.value,
-      date: Form.date.value
-    }
-  },
-  validateField() {
-    const { description, amount, date } = Form.getValues()
-    if (
-      description.trim() === '' ||
-      amount.trim() === '' ||
-      date.trim() === ''
-    ) {
-      throw new Error('Por favor, preencha todos os campos.')
-    }
-  },
-  formatValues() {
-    let { description, amount, date } = Form.getValues()
-    amount = Utils.formatAmount(amount)
-    date = Utils.formatDate(date)
+    getValues() {
+        return {
+            description: Form.description.value,
+            amount: Form.amount.value,
+            date: Form.date.value
+        }
+    },
+    validateField() {
+        const { description, amount, date } = Form.getValues()
+        if (
+            description.trim() === '' ||
+            amount.trim() === '' ||
+            date.trim() === ''
+        ) {
+            throw new Error('Por favor, preencha todos os campos.')
+        }
+    },
+    formatValues() {
+        let { description, amount, date } = Form.getValues()
+        amount = Utils.formatAmount(amount)
+        date = Utils.formatDate(date)
 
-    return { description, amount, date }
-  },
-  clearFields() {
-    Form.description.value = ''
-    Form.amount.value = ''
-    Form.date.value = ''
-  },
-  submit(event) {
-    event.preventDefault()
+        return { description, amount, date }
+    },
+    clearFields() {
+        Form.description.value = ''
+        Form.amount.value = ''
+        Form.date.value = ''
+    },
+    submit(event) {
+        event.preventDefault()
 
-    try {
-      Form.validateField()
-      const transaction = Form.formatValues()
-      Transaction.add(transaction)
-      Form.clearFields()
-      Modal.toggle('.modal-overlay')
-    } catch (error) {
-      alert(error.message)
+        try {
+            Form.validateField()
+            const transaction = Form.formatValues()
+            Transaction.add(transaction)
+            Form.clearFields()
+            Modal.toggle('.modal-overlay')
+        } catch (error) {
+            alert(error.message)
+        }
     }
-  }
 }
 
 const App = {
-  init() {
-    Transaction.all.forEach(DOM.addTransaction)
-    DOM.updateBalance()
-    Storage.set(Transaction.all)
-  },
-  reload() {
-    DOM.clearTransactions()
-    App.init()
-  }
+    init() {
+        Transaction.all.forEach(DOM.addTransaction)
+        DOM.updateBalance()
+        Storage.set(Transaction.all)
+
+        let theme = localStorage.getItem('dev.finances:theme') || 'green',
+            DM = localStorage.getItem('dev.finances:dark-mode') || 'off',
+            dmInput = document.getElementById('darkMode')
+
+        dmInput.checked = DM == 'on' ? true : false
+        document.documentElement.setAttribute('dark-mode', DM)
+
+        document.querySelector('#DM label').innerHTML =
+            DM == 'on' ? 'Ligado' : 'Desligado'
+
+        dmInput.addEventListener('click', e => {
+            document.documentElement.setAttribute(
+                'dark-mode',
+                e.target.checked ? 'on' : 'off'
+            )
+            document.querySelector('#DM label').innerHTML = e.target.checked
+                ? 'Ligado'
+                : 'Desligado'
+            localStorage.setItem(
+                'dev.finances:dark-mode',
+                e.target.checked ? 'on' : 'off'
+            )
+        })
+
+        document.documentElement.setAttribute('data-theme', theme)
+    },
+    reload() {
+        DOM.clearTransactions()
+        this.init()
+    }
 }
 
-function setTheme(theme) {
-  document.documentElement.setAttribute('data-theme', theme)
-}
+const Custom = {
+    setTheme(theme) {
+        document.documentElement.setAttribute('data-theme', theme)
+        localStorage.setItem('dev.finances:theme', theme)
+    },
 
-function darkMode() {
-  let selection = document.getElementById('darkMode')
-
-  if (selection.checked) {
-    document.documentElement.setAttribute('dark-mode', 'on')
-    document.querySelector('.dark-mode label').innerHTML = 'Ligado'
-  } else {
-    document.documentElement.setAttribute('dark-mode', 'off')
-    document.querySelector('.dark-mode label').innerHTML = 'Desligado'
-  }
+    toggleDM(darkmode) {
+        document.documentElement.setAttribute(
+            'dark-mode',
+            darkmode ? 'on' : 'off'
+        )
+        document.querySelector('#DM label').innerHTML = darkmode
+            ? 'Ligado'
+            : 'Desligado'
+        localStorage.setItem('dev.finances:dark-mode', darkmode)
+    }
 }
 
 App.init()
